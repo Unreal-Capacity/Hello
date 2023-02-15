ef7ac03448dedcb8dae7a97895ed1db39b67dbc7
Unreal Capacity <123221098+Unreal-Capacity@users.noreply.github.com>
2023-02-15 10:52:46 +0800

    latest

---
+++ hello.js
@@ -0,0 +1,21 @@
+/*let pi = 3.14159;
+let radius;
+let circumfrence;
+
+radius = window.prompt("Enter the Radius");
+radius = Number(radius);
+
+circumfrence = 2*pi*radius
+
+console.log(`The circumfrence is: ${circumfrence}`)
+
+let x = 3.24;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x,3);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+x = Math.PI
+console.log(x);*/
